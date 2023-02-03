dac2960e4932573b076f4b76fa28f4fe4e874149
Riley Kinahan <kinahanriley@gmail.com>
2023-02-03 14:29:48 -0700

    Revert "Formatting fixes"
    This reverts commit 7ed1f6b5875ce83df31dd496eb04e32bb6ceb385.

--- application/static/scripts.js
+++ application/static/scripts.js
@@ -1,62 +1,61 @@
 function showPopup() {
     document.getElementById("popup").classList.remove("hidden");
-}
-function hidePopup() {
+  }
+  function hidePopup() {
     document.getElementById("popup").classList.add("hidden");
-}
-function delayedHidePopup() {
+  }
+  function delayedHidePopup() {
     timer = setTimeout(hidePopup, 500);              
-}
-function delayHidePopup() {
+  }
+  function delayHidePopup() {
     clearTimeout(timer);
-}
+  }
 
-const transition_elements = document.querySelectorAll('.fade_in, .flow_in_left, .flow_in_top');
+  const transition_elements = document.querySelectorAll('.fade_in, .flow_in_left, .flow_in_top');
 
-function isInViewport(el) {
+  function isInViewport(el) {
     // checks whether an elements bounding box is completely within the vertical space of the window
     const rect = el.getBoundingClientRect();  // rect is the bounding box of the element
     return (
-        // compare rects top and bottom to the top and bottom of the window
-        rect.top >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+      // compare rects top and bottom to the top and bottom of the window
+      rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
     );
-}
+  }
 
-function checkElements() {
+  function checkElements() {
     // checks if each element is within the window and makes it visible if it is
     transition_elements.forEach((el) => {
-        if (isInViewport(el)) {
+      if (isInViewport(el)) {
         el.classList.add('visible');
-        }
+      }
     });
-}
+  }
+  // run checkElements on page load
+  checkElements();
+  // run each time the user scrolls
+  window.addEventListener('scroll', checkElements);
 
-// run checkElements on page load
-checkElements();
-// run each time the user scrolls
-window.addEventListener('scroll', checkElements);
-
-function hideHeader() {
+  function hideHeader() {
     const scrollThreshold = 200;
     if (window.scrollY > scrollThreshold) {
-        header = document.getElementById('header');
-        header.classList.add('header_hidden');
+      header = document.getElementById('header');
+      header.classList.add('header_hidden');
     }
-}
+  }
 
-window.addEventListener('scroll', hideHeader);
+  window.addEventListener('scroll', hideHeader);
 
-function showHeader() {
+  function showHeader() {
     if (window.scrollY <= 0) {
-        header = document.getElementById('header');
-        header.classList.remove('header_hidden');
+      header = document.getElementById('header');
+      header.classList.remove('header_hidden');
     }
-}
-window.addEventListener('scroll', showHeader);
+  }
+  window.addEventListener('scroll', showHeader);
 
   
-function moveBkg() {
+  function moveBkg() {
     var value = window.scrollY;
     bkg = document.getElementById('bg-image');
     rect = bkg.getBoundingClientRect();
@@ -65,7 +64,7 @@ function moveBkg() {
     distance = window.innerHeight - rect.top;
     percent = (1.0 * distance) / (rect.bottom - rect.top);
     if (percent >= 1){
-        percent = 1;  
+      percent = 1;  
     }
 
     pos_x = - (extra * percent);
@@ -74,7 +73,7 @@ function moveBkg() {
     postion_string = pos_x + 'px 50%'
 
     bkg.style.backgroundPosition = postion_string;
-};
+  };
   
-window.addEventListener('scroll', moveBkg);
-window.addEventListener('resize', moveBkg);
+  window.addEventListener('scroll', moveBkg);
+  window.addEventListener('resize', moveBkg);
