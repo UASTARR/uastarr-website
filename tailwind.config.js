885044a4a9ebbe73f17ecb1d321a49276a7b0315
Raymart <raymartd1@gmail.com>
2023-02-12 22:50:39 -0700

    I think now I fixed the merges

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./application/templates/**/*.html',],
-  theme: {
-    extend: {
-      transitionDelay: {
-        '0': '0ms',
-        '400': '400ms',
-        '600': '600ms',
-        '800': '800ms',
-        '900': '900ms',
-        '1200': '1200ms',
-
-      },
-      spacing: {
-        '192': '48rem',
-        '224': '56rem',
-        '256':'64rem',
-        '288': '72rem',
-      },
-    },
-  },
-  plugins: [],
-}
-=======
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./application/templates/**/*.html',],
@@ -52,4 +26,3 @@ module.exports = {
   },
   plugins: [],
 }
->>>>>>> master
