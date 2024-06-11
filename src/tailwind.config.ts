40ec72e5618410878795184803b6595467dd1b6b
Brian Lin <shihhsu1@ualberta.ca>
2024-06-10 20:28:03 -0600

    Next js init

---
+++ src/tailwind.config.ts
@@ -0,0 +1,43 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'DarkBlue': '#02060f',
+      },
+      transitionDelay: {
+        '0': '0ms',
+        '400': '400ms',
+        '600': '600ms',
+        '800': '800ms',
+        '900': '900ms',
+        '1200': '1200ms',
+
+      },
+      spacing: {
+        '112': '28rem',
+        '128': '32rem',
+        '144': '36rem',
+        '160': '40rem',
+        '192': '48rem',
+        '224': '56rem',
+        '256':'64rem',
+        '288': '72rem',
+        '448': '112rem',
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
