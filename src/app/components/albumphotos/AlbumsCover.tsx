6c7b08d2d57563f965cc19c197acf5fb219a1f0c
Brian Lin <shihhsu1@ualberta.ca>
2024-06-16 01:41:50 -0600

    chore: update background colour

--- src/app/components/albumphotos/AlbumsCover.tsx
+++ src/app/components/albumphotos/AlbumsCover.tsx
@@ -15,7 +15,7 @@ const AlbumsCover = async () => {
                         <Link href={`/photo-albums/${album}`}>
                             <Image className="object-cover w-60 h-80 hover:blur-sm rounded-xl hover:object-scale-down hover:bg-neutral-900" src={photos[album][0].url} width={500} height={500} alt={photos[album][0].name} />
                         </Link>
-                        <div className="absolute bottom-3 opacity-80 bg-gradient-to-r from-violet-700 to-sky-700 h-20 w-56 py-3 px-3 place-content-center ">
+                        <div className="absolute bottom-3 bg-gradient-to-r from-violet-700 to-sky-700/75 h-20 w-56 py-3 px-3 place-content-center ">
                             <p className="text-white text-lg font-bold text-center align-middle">{album}</p>
                         </div>
                     </div>
