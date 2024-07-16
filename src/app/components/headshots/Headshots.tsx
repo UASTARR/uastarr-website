af9cfe2d437c42d3814e8719bdda48db8e93bc05
Brian <116841669+Brian-333@users.noreply.github.com>
2024-07-16 17:21:14 -0600

    chore: Update headshots and their fit

--- src/app/components/headshots/Headshots.tsx
+++ src/app/components/headshots/Headshots.tsx
@@ -6,7 +6,7 @@ const Headshots = async () => {
         <div className="flex flex-wrap justify-center">
             {headshots.map((headshot: any, index: number) => (
                 <div key={index} className="w-80 h-112 flex flex-col items-center justify-center relative">
-                    <img src={headshot.imgref} alt={headshot.name} />
+                    <img src={headshot.imgref} alt={headshot.name} className="w-[245px] h-[323px] object-cover"/>
                     <div className="absolute right-6 bottom-6 bg-green-700 h-20 w-52 py-3 px-3">
                         <p className="text-white text-lg font-bold">{headshot.name}</p>
                         <div className="h-1"></div>
