d9ffce9f9c252679ff3247f18a39a8eaaa2d436c
Brian Lin <shihhsu1@ualberta.ca>
2024-08-11 19:57:03 -0600

    NextJS Cache Test

--- src/app/photo-albums/page.tsx
+++ src/app/photo-albums/page.tsx
@@ -4,7 +4,6 @@ import AlbumsCover from '../components/albumphotos/AlbumsCover'
 
 import { Metadata } from 'next';
 
-export const dynamic = 'force-dynamic';
 
 export const metadata: Metadata = {
     title: "Photo Albums",
