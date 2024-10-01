4b760dad12b642be8283511d3295b0e489917b69
Brian Lin <shihhsu1@ualberta.ca>
2024-10-01 13:58:17 -0600

    fix: joined redirects into nextconfig

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -1,28 +1,33 @@
-/** @type {import('next').NextConfig} */
-export default {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'static.wixstatic.com',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-                pathname: '**',
-            },
-        ]
-    },
-};
+// @ts-check
 
-export async function redirects() {
-    return [
-        {
-            source: '/join',
-            destination: 'https://forms.gle/rjTRr46NLjMoaXFMA',
-            permanent: true,
+/**
+* @type {import('next').NextConfig}
+*/
+export default (phase, { defaultConfig }) => {
+    return {
+        images: {
+            remotePatterns: [
+                {
+                    protocol: 'https',
+                    hostname: 'static.wixstatic.com',
+                    pathname: '**',
+                },
+                {
+                    protocol: 'https',
+                    hostname: 'firebasestorage.googleapis.com',
+                    pathname: '**',
+                },
+            ]
         },
-    ]
-}
+        redirects: async () => {
+            return [
+                {
+                    source: '/join',
+                    destination: 'https://forms.gle/rjTRr46NLjMoaXFMA',
+                    permanent: true,
+                },
+            ]
+        },
+    };
+};
 
