b4bed0a31c231aff4aa709e1782e76a62f7f49d1
jake158 <kulikovpavel2801@gmail.com>
2025-01-05 03:13:36 -0700

    feat: move form to separate client component, use zod for validation

---
+++ src/app/components/contact/ContactForm.tsx
@@ -0,0 +1,203 @@
+'use client';
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const schema = z.object({
+  firstname: z.string().min(1, 'First name is required'),
+  lastname: z.string().min(1, 'Last name is required'),
+  email: z.string().email('Invalid email address'),
+  subject: z.enum(
+    [
+      'member_inquiry',
+      'sponsorship_inquiry',
+      'donation_inquiry',
+      'event_inquiry',
+      'other',
+    ],
+    { errorMap: () => ({ message: 'Please select a subject' }) }
+  ),
+  know: z.string().optional(),
+  subscribe: z.boolean().optional(),
+  message: z.string().optional(),
+});
+
+export type FormSchema = z.infer<typeof schema>;
+
+const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSchema>({
+    resolver: zodResolver(schema),
+  });
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const onSubmit: SubmitHandler<FormSchema> = async (data) => {
+    console.log('Submitting form:', data);
+
+    try {
+      const response = await fetch('/api/submit-form', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        console.log('Form submitted successfully');
+      } else {
+        const errorData = await response.json();
+        console.error('Error submitting form:', errorData.error);
+      }
+    } catch (error) {
+      console.error('Network error submitting form:', error);
+    }
+  };
+
+  return (
+    <div className="max-w-2xl mx-auto bg-white text-black p-6 rounded-lg shadow-lg">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+        <div className="flex flex-wrap gap-4">
+          <div className="flex-1">
+            <label htmlFor="firstname" className="block font-medium">
+              First Name
+            </label>
+            <input
+              type="text"
+              id="firstname"
+              {...register('firstname')}
+              className="w-full py-2 border-b border-gray-300 focus:outline-none focus:border-yellow-500"
+            />
+            {errors.firstname && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.firstname.message}
+              </p>
+            )}
+          </div>
+          <div className="flex-1">
+            <label htmlFor="lastname" className="block font-medium">
+              Last Name
+            </label>
+            <input
+              type="text"
+              id="lastname"
+              {...register('lastname')}
+              className="w-full py-2 border-b border-gray-300 focus:outline-none focus:border-yellow-500"
+            />
+            {errors.lastname && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.lastname.message}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="email" className="block font-medium">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            {...register('email')}
+            className="w-full py-2 border-b border-gray-300 focus:outline-none focus:border-yellow-500"
+          />
+          {errors.email && (
+            <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>
+          )}
+        </div>
+
+        <div>
+          <label htmlFor="subject" className="block font-medium pb-3">
+            Subject
+          </label>
+          <select
+            id="subject"
+            {...register('subject')}
+            className="w-full p-2 border-b border-gray-300 focus:outline-none focus:border-yellow-500"
+          >
+            <option value="" disabled hidden>
+              Choose an option
+            </option>
+            <option value="member_inquiry">
+              Membership Application Inquiry
+            </option>
+            <option value="sponsorship_inquiry">Sponsorship Inquiry</option>
+            <option value="donation_inquiry">Donation Inquiry</option>
+            <option value="event_inquiry">Event Inquiry</option>
+            <option value="other">Other</option>
+          </select>
+          {errors.subject && (
+            <p className="text-red-500 text-sm mt-1">
+              {errors.subject.message}
+            </p>
+          )}
+        </div>
+
+        <div>
+          <label htmlFor="know" className="block font-medium">
+            How did you hear about us?
+          </label>
+          <input
+            type="text"
+            id="know"
+            {...register('know')}
+            className="w-full py-2 border-b border-gray-300 focus:outline-none focus:border-yellow-500"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="message" className="block font-medium">
+            Leave a Message
+          </label>
+          <textarea
+            id="message"
+            {...register('message')}
+            rows={5}
+            className="w-full py-2 border-b border-gray-300 focus:outline-none focus:border-yellow-500"
+          ></textarea>
+          {errors.message && (
+            <p className="text-red-500 text-sm mt-1">
+              {errors.message.message}
+            </p>
+          )}
+        </div>
+
+        <div className="text-center">
+          <label className="inline-flex items-center">
+            <input
+              type="checkbox"
+              {...register('subscribe')}
+              className="mr-2"
+            />
+            Subscribe to the STARR newsletter
+          </label>
+
+          {isClient && (
+            <div
+              className="g-recaptcha mt-4 flex justify-center"
+              data-sitekey="6Leo32UpAAAAAJmvvWFtlNfapVA2bn_qxHIbO77J"
+            ></div>
+          )}
+
+          <button
+            type="submit"
+            className="mt-4 px-6 py-3 bg-yellow-500 text-white font-medium rounded hover:bg-yellow-600"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,36 +1,17 @@
 import React from 'react';
+import { Metadata } from 'next';
 import BaseScripts from '@/app/components/scripts/BaseScripts';
 import ReCaptchaApi from '@/app/components/scripts/ReCaptchaApi';
-import { addResponse } from '@/library/google/api.js';
-import { Metadata } from 'next';
+import ContactForm from '../components/contact/ContactForm';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Contact Us',
 };
 
-const ContactPage = () => {
-  async function submitForm(formData: FormData) {
-    'use server';
-
-    const data = {
-      firstName: formData.get('firstname'),
-      lastName: formData.get('lastname'),
-      email: formData.get('email'),
-      subject: formData.get('subject'),
-      know: formData.get('know'),
-      subscribe: formData.get('subscribe'),
-      message: formData.get('message'),
-    };
-
-    const dataArray = Object.values(data);
-
-    if (await addResponse(dataArray)) {
-      console.log('Message sent successfully!');
-    } else {
-      console.log('An error occurred. Please try again later.');
-    }
-    console.log(data);
-  }
+const ContactPage: React.FC = () => {
+  // TODO: temporary
+  redirect('/down-for-maintenance');
 
   return (
     <main className="relative bg-gray-900 text-white">
@@ -56,121 +37,9 @@ const ContactPage = () => {
         <p className="mt-2">Let us know! We'd love to hear what you think.</p>
       </div>
 
-      {/* Contact Form */}
+      {/* Contact Form - Client component */}
       <div className="relative z-10 px-4">
-        <div className="max-w-2xl mx-auto bg-white text-black p-6 rounded-lg shadow-lg">
-          <form action={submitForm} className="space-y-6">
-            <div className="flex flex-wrap gap-4">
-              <div className="flex-1">
-                <label htmlFor="fname" className="block font-medium">
-                  First Name
-                </label>
-                <input
-                  type="text"
-                  id="fname"
-                  name="firstname"
-                  required
-                  className="w-full py-2 border-b border-gray-300 focus:outline-none focus:border-yellow-500"
-                />
-              </div>
-              <div className="flex-1">
-                <label htmlFor="lname" className="block font-medium">
-                  Last Name
-                </label>
-                <input
-                  type="text"
-                  id="lname"
-                  name="lastname"
-                  required
-                  className="w-full py-2 border-b border-gray-300 focus:outline-none focus:border-yellow-500"
-                />
-              </div>
-            </div>
-
-            <div>
-              <label htmlFor="email" className="block font-medium">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                required
-                className="w-full py-2 border-b border-gray-300 focus:outline-none focus:border-yellow-500"
-              />
-            </div>
-
-            <div>
-              <label htmlFor="subject" className="block font-medium pb-3">
-                Subject
-              </label>
-              <select
-                id="subject"
-                name="subject"
-                required
-                className="w-full p-2 border-b border-gray-300 focus:outline-none focus:border-yellow-500"
-              >
-                <option value="" disabled hidden>
-                  Choose an option
-                </option>
-                <option value="member_inquiry">
-                  Membership Application Inquiry
-                </option>
-                <option value="sponsorship_inquiry">Sponsorship Inquiry</option>
-                <option value="donation_inquiry">Donation Inquiry</option>
-                <option value="event_inquiry">Event Inquiry</option>
-                <option value="other">Other</option>
-              </select>
-            </div>
-
-            <div>
-              <label htmlFor="know" className="block font-medium">
-                How did you hear about us?
-              </label>
-              <input
-                type="text"
-                id="know"
-                name="know"
-                className="w-full py-2 border-b border-gray-300 focus:outline-none focus:border-yellow-500"
-              />
-            </div>
-
-            <div>
-              <label htmlFor="message" className="block font-medium">
-                Leave a Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                rows={5}
-                className="w-full py-2 border-b border-gray-300 focus:outline-none focus:border-yellow-500"
-              ></textarea>
-            </div>
-
-            <div className="text-center">
-              <label className="inline-flex items-center">
-                <input
-                  type="checkbox"
-                  id="subscribe"
-                  name="subscribe"
-                  value="newsletter"
-                  className="mr-2"
-                />
-                Subscribe to the STARR newsletter
-              </label>
-              <div
-                className="g-recaptcha mt-4 flex justify-center"
-                data-sitekey="6Leo32UpAAAAAJmvvWFtlNfapVA2bn_qxHIbO77J"
-              ></div>
-              <button
-                type="submit"
-                className="mt-4 px-6 py-3 bg-yellow-500 text-white font-medium rounded hover:bg-yellow-600"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
+        <ContactForm />
       </div>
 
       {/* Banner */}
