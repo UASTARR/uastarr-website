a1cf1208eaf0b4857c63995450b3b2094f739e97
Brian Lin <shihhsu1@ualberta.ca>
2026-01-04 23:20:24 -0700

    fix: env var name

--- src/app/components/headshots/Headshots.tsx
+++ src/app/components/headshots/Headshots.tsx
@@ -8,7 +8,7 @@ const Headshots = async () => {
         <div className="flex flex-wrap justify-center">
             {members.map((member: any, index: number) => (
                 <div key={index} className="w-80 h-112 flex flex-col items-center justify-center relative">
-                    <Image src={member.img_ref_link === null ? process.env.DEFAULT_MEMBER_IMAGE_URL! : member.img_ref_link} alt={`${member.first_name} ${member.last_name}`} className="w-[245px] h-[323px] object-cover" width={500} height={500} />
+                    <Image src={member.img_ref_link === null ? process.env.default_member_image_url! : member.img_ref_link} alt={`${member.first_name} ${member.last_name}`} className="w-[245px] h-[323px] object-cover" width={500} height={500} />
                     <div className="absolute right-6 bottom-6 bg-green-700 w-52 py-3 px-3">
                         <p className="text-white text-lg font-bold">{member.first_name} {member.last_name}</p>
                         <div className="h-1"></div>
--- src/library/neon/database.ts
+++ src/library/neon/database.ts
@@ -3,14 +3,14 @@ import { neon } from '@neondatabase/serverless';
 
 // Sponsors
 export async function getSponsors() {
-    const sql = neon(process.env.NEON_DB_CONNECTION_URL!);
+    const sql = neon(process.env.neon_db_connection_url!);
     const rows = await sql`SELECT * FROM sponsors`;
     return Response.json({rows});
 }
 
 // Members
 export async function getActiveExecutives() {
-    const sql = neon(process.env.NEON_DB_CONNECTION_URL!);
+    const sql = neon(process.env.neon_db_connection_url!);
     const all_executives = await sql`SELECT * FROM members WHERE display_order IS NOT NULL AND active = TRUE ORDER BY display_order ASC`;
     const other_members = await sql`SELECT * FROM members WHERE display_order IS NULL AND active = TRUE ORDER BY title ASC, first_name ASC`;
     all_executives.push(...other_members);
