124042faa56c65542a09a14d4930cc06e64ec5b8
Brian Lin <shihhsu1@ualberta.ca>
2026-05-22 18:02:54 +0000

    feat: add event banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,6 +82,26 @@ export default function Home() {
             {/* <!--Tile 2 Background Video--> */}
             <PrintingBackground />
 
+            {/* <!-- Event Banner --> */}
+            <div className="z-20 relative flex flex-nowrap py-6 flex-col bg-local bg-cover bg-left justify-between bg-yellow-500">
+                {/* <!--Main Text--> */}
+                <div className="px-5 lg:px-20">
+                    <div className="flex flex-col lg:flex-col items-center">
+                        <h1 className="text-black text-center text-3xl lg:text-5xl font-bold flow_in_left pb-5">
+                            Help us reach for the STARRs
+                        </h1>
+                        <div className="text-black text-center text-xl lg:text-3xl flow_in_left leading-[1] lg:leading-[1.5] pb-3">
+                            <span className="block">
+                                Support our crowdfunding campaign to help us reach new heights!
+                            </span>
+                            <span className="block"> 
+                                <Link target="_blank" href="https://ualberta.alumniq.com/crowdfunding/Crowdfunding/starrcf" rel="noopener noreferrer" className="underline hover:text-blue-600">Donate Here</Link>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             {/* <!--Tile 2--> */}
             <div className="z-10 relative flex flex-nowrap h-192 flex-col bg-emerald-900 bg-opacity-10 bg-local bg-cover bg-[center-10rem] justify-between">
                 <div className="flex pl-3 lg:pl-20 overflow-hidden">
@@ -237,7 +257,7 @@ export default function Home() {
                             logo={{ src: launchCanadaLogo, alt: "Launch Canada Logo" }}
                             description="Launch Canada is a rocket competition designed to create
                             opportunities for amateur rocketeers from Canadian
-                            universities. We plan to launch Ringo V."
+                            universities. We plan to launch Alberio."
                         />
                     </div>
 
